12d732bdf8b5a47333723c718522b5d80ab3770c
Sydes <huangsteeve9@gmail.com>
2025-02-04 15:09:50 +0100

    Prix

--- DataScraper.py
+++ DataScraper.py
@@ -3,8 +3,11 @@ import requests
 
 class NonValide(Exception):
 
-    def __str__():
-        return "Annonce non valide"
+    def __init__(self, raison):
+        self.raison = raison
+
+    def __str__(self):
+        return f"Annonce non valide: {self.raison}"
 
 def getsoup(url):
     rhtml = requests.get(url, auth=('user', 'pass')).text
@@ -12,3 +15,13 @@ def getsoup(url):
 
     return soup
 
+def prix(soup):
+
+    prixComplet = soup.find('p', class_='product-price').text
+    prix = prixComplet.replace(' €', '').replace(' ', '')
+
+    if (int(prix) < 10000):
+        raise NonValide("Prix < 10000")
+
+    return prix
+ 
--- Main.py
+++ Main.py
@@ -1,5 +1,10 @@
 # https://www.immo-entre-particuliers.com
+# https://www.immo-entre-particuliers.com/annonce-yvelines-mantes-la-jolie/409301-studio-meuble
 
 from DataScraper import *
 
-print(getsoup("https://www.immo-entre-particuliers.com"))
+soup = getsoup("https://www.immo-entre-particuliers.com/annonce-yvelines-mantes-la-jolie/409301-studio-meuble")
+print(prix(soup))
+
+soup2 = getsoup("https://www.immo-entre-particuliers.com/annonce-paris-paris-1er/408780-hotel-a-vendre")
+print(prix(soup2))
