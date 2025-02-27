5412877b57ace313d716d0399e0c51f1660f3c77
Sydes <46070813+0Sydes@users.noreply.github.com>
2025-02-27 16:02:23 +0100

    final ver

--- Main.py
+++ Main.py
@@ -3,5 +3,19 @@
 # https://www.immo-entre-particuliers.com/annonce-yvelines-mantes-la-jolie/409301-studio-meuble
 
 from DataScraper import *
+from DataCleaning import *
 
-scrapLink("https://www.immo-entre-particuliers.com/annonces/france-ile-de-france/vente/ta-offer")
+# Partie 1: Data Scraping
+# scrapLink("https://www.immo-entre-particuliers.com/annonces/france-ile-de-france/vente/ta-offer")
+
+# Partie 2: Data Cleaning
+annonces = read_csv('data.csv', encoding='latin1')
+annonces['DPE'] = annonces['DPE'].replace('-', 'Vierge')
+villes = read_csv('cities.csv')
+
+clean_all(annonces)
+annonces = splitMergeAll(annonces)
+annonces = mergeVille(annonces, villes)
+annonces = annonces.dropna()
+
+print(annonces.to_string())
