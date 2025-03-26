93ea370c54a83855f14eaddf80a3b6959b9c0014
Slashinkun <ktang3214@gmail.com>
2025-03-26 16:55:40 +0100

    Changement fct apprentisage
    Co-authored-by: Sydes <0Sydes@users.noreply.github.com>

--- ImmoIA.py
+++ ImmoIA.py
@@ -30,32 +30,34 @@ annonces = annonces.dropna()
 
 X = annonces.drop("Prix",axis="columns")
 y = annonces["Prix"]
-y_pred = []
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=49, train_size=0.75,test_size=0.25)
 
 
 def apprentissage(X_train,X_test):
 
+    scores = {}
+    y_pred = {}
+
     #Regression Linéaire :
 
     lin_reg = linear_model.LinearRegression()
     lin_reg.fit(X_train,y_train)
-    prediction=lin_reg.predict(X_test)
-    score = lin_reg.score(X_test,y_test)
-    #print(y_test.shape)
-    #print(prediction.shape)
-    print("Sans pretraitement + LR : ",score)
-    y_pred.append(prediction)
+    pred_lr = lin_reg.predict(X_test)
+    score_lr = lin_reg.score(X_test,y_test)
+    print("Sans pretraitement + LR :", score_lr)
+    scores["LR"] = score_lr
+    y_pred["LR"] = pred_lr
 
 
     #StandardScaler + LinearRegression
     scale_lr = make_pipeline(StandardScaler(),linear_model.LinearRegression())
     scale_lr.fit(X_train,y_train)
     scaled_pred = scale_lr.predict(X_test)
-    score_scaled = scale_lr.score(X_test,y_test)
-    print("StandardScaler + LR : ",score_scaled)
-    y_pred.append(scaled_pred)
+    score_std = scale_lr.score(X_test,y_test)
+    print("StandardScaler + LR :", score_std)
+    scores['Standardisation + LR'] = score_std
+    y_pred["Standardisation + LR"] = scaled_pred
 
 
     #MinMaxScaler + LinearRegression
@@ -63,63 +65,81 @@ def apprentissage(X_train,X_test):
     minmax_lr.fit(X_train,y_train)
     minmax_lr_pred = minmax_lr.predict(X_test)
     score_minmax = minmax_lr.score(X_test,y_test)
-    print("MinMax + LR : ",score_minmax)
-    y_pred.append(minmax_lr_pred)
+    print("MinMax + LR",score_minmax)
+    y_pred["MinMaxScaler + LinearRegression"] = minmax_lr_pred
+    scores["Normalisation + LR"] = score_minmax
     print("\n")
 
     #Arbre de décision :
 
-    for i in range(4,6):
+    #DecisionTreeRegressor
+    
+    decision_tr = DecisionTreeRegressor(max_depth=4)
+    decision_tr.fit(X_train,y_train)
+    pred_dtr = decision_tr.predict(X_test)
+    score_dtr = decision_tr.score(X_test,y_test)
+    scores[f"AD"] = score_dtr
+    print(f"AD : ",score_dtr)
+    y_pred["AD"] = score_dtr
 
-        #DecisionTreeRegressor
-        decision_tr = DecisionTreeRegressor(max_depth=i)
-        decision_tr.fit(X_train,y_train)
-        pred_dtr = decision_tr.predict(X_test)
-        score_dtr = decision_tr.score(X_test,y_test)
-        print(f"AD {i} : ",score_dtr)
 
+    #StandardScaler + DecisionTreeRegressor
+    std_dtr = make_pipeline(StandardScaler(), DecisionTreeRegressor(max_depth=4))
+    std_dtr.fit(X_train,y_train)
+    std_dtr_pred = std_dtr.predict(X_test)
+    score_std_dtr = std_dtr.score(X_test,y_test)
+    scores[f"Standardisation + AD"] = score_std_dtr
+    print(f"StandardScaler + AD  :", score_std_dtr)
+    y_pred["Standardisation + AD"] = std_dtr
 
-        #StandardScaler + DecisionTreeRegressor
-        std_dtr = make_pipeline(StandardScaler(),DecisionTreeRegressor(max_depth=i))
-        std_dtr.fit(X_train,y_train)
-        std_dtr_pred = std_dtr.predict(X_test)
-        score_std_dtr = std_dtr.score(X_test,y_test)
-        print(f"StandardScaler + AD {i} : ",score_std_dtr)
+    #MinMaxScaler + DecisionTreeRegressor
+    minmax_dtr = make_pipeline(MinMaxScaler(), DecisionTreeRegressor(max_depth=4))
+    minmax_dtr.fit(X_train,y_train)
+    minmax_dtr_pred = minmax_dtr.predict(X_test)
+    score_minmax_dtr = minmax_dtr.score(X_test,y_test)
+    scores[f"Normalisation + AD"] = score_minmax_dtr
+    print(f"MinMaxScaler + AD  :", score_minmax_dtr)
+    y_pred["Normalisation + AD"] = minmax_dtr_pred
 
-        #MinMaxScaler + DecisionTreeRegressor
-        minmax_dtr = make_pipeline(MinMaxScaler(),DecisionTreeRegressor(max_depth=i))
-        minmax_dtr.fit(X_train,y_train)
-        minmax_dtr_pred = minmax_dtr.predict(X_test)
-        score_minmax_dtr = minmax_dtr.score(X_test,y_test)
-        print(f"MinMaxScaler + AD {i} : ",score_minmax_dtr)
     print("\n")
 
     #N plus proches voisins :
 
-    for i in range(4,6):
+    k_neigbors = KNeighborsRegressor(n_neighbors=5)
+    k_neigbors.fit(X_train,y_train)
+    k_neigbors_pred = k_neigbors.predict(X_test)
+    score_kn = k_neigbors.score(X_test,y_test)
+    scores[f"KNN"] = score_kn
+    print(f"KNeighborsR :", score_kn)
+    y_pred["KNN"] = k_neigbors_pred
 
-        k_neigbors = KNeighborsRegressor(n_neighbors=i)
-        k_neigbors.fit(X_train,y_train)
-        k_neigbors_pred = k_neigbors.predict(X_test)
-        score_kn = k_neigbors.score(X_test,y_test)
-        print(f"KNeighborsR {i} : " , score_kn)
 
+    k_neigbors_std = make_pipeline(StandardScaler(),KNeighborsRegressor(n_neighbors=5))
+    k_neigbors_std.fit(X_train,y_train)
+    k_neigbors_std_pred = k_neigbors_std.predict(X_test)
+    k_neigbors_std_score = k_neigbors_std.score(X_test,y_test)
+    scores[f"Standardisation + KNN"] = k_neigbors_std_score
+    print(f"StandardScaler + KNeighborsR :", k_neigbors_std_score)
+    y_pred["Standardisation + KNN"] = k_neigbors_std_pred
 
-        k_neigbors_std = make_pipeline(StandardScaler(),KNeighborsRegressor(n_neighbors=i))
-        k_neigbors_std.fit(X_train,y_train)
-        k_neigbors_std_pred = k_neigbors_std.predict(X_test)
-        k_neigbors_std_score = k_neigbors_std.score(X_test,y_test)
-        print(f"StandardScaler + KNeighborsR {i} : ",k_neigbors_std_score)
-
-        k_neigbors_minmax = make_pipeline(MinMaxScaler(),KNeighborsRegressor(n_neighbors=i))
-        k_neigbors_minmax.fit(X_train,y_train)
-        k_neigbors_minmax_pred = k_neigbors_minmax.predict(X_test)
-        k_neigbors_minmax_score = k_neigbors_minmax.score(X_test,y_test)
-        print(f"MinMaxScaler + KNeighborsR {i} : ",k_neigbors_minmax_score)
+    k_neigbors_minmax = make_pipeline(MinMaxScaler(),KNeighborsRegressor(n_neighbors=5))
+    k_neigbors_minmax.fit(X_train,y_train)
+    k_neigbors_minmax_pred = k_neigbors_minmax.predict(X_test)
+    k_neigbors_minmax_score = k_neigbors_minmax.score(X_test,y_test)
+    scores[f"Normalisation + KNN"] = k_neigbors_minmax_score
+    print(f"MinMaxScaler + KNeighborsR :", k_neigbors_minmax_score)
     print("\n")
+    y_pred["Normalisation + KNN"] = k_neigbors_minmax_pred
+
+    return scores, y_pred
 
 
-apprentissage(X_train,X_test)
+scores, y_pred = apprentissage(X_train, X_test)
+
+
+
+
+
 #A faire : Visualisation des résultats
 
 # plt.plot(np.array(y_pred),np.array(y_test))
@@ -134,9 +154,9 @@ X_train_pca = pca.transform(X_train)
 X_test_pca = pca.transform(X_test)
    
 
-print("Avec PCA à 2 composants:\n")
+#print("Avec PCA à 2 composants:\n")
 
-apprentissage(X_train_pca,X_test_pca)
+# apprentissage(X_train_pca,X_test_pca)
 
 
 
